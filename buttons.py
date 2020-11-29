a8fb6b605e130abfa302f6e85809eae6c8553b8c
Alex-2kZharkov <Alexander.06.zharkov@gmail.com>
2020-11-29 15:39:39 +0600

    Текущая версия с гифкой и кнпокой

---
+++ buttons.py
@@ -0,0 +1,11 @@
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.utils.callback_data import CallbackData
+
+action_callback = CallbackData("action", "action_type")
+
+
+button_start = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [ InlineKeyboardButton(text="Начать тест⚡️", callback_data="action:start")]
+    ]
+)
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import json
 import logging
 from config import API_TOKEN
 from aiogram import Bot, Dispatcher, executor, types
+from buttons import button_start
 
 
 logging.basicConfig(level=logging.INFO)
@@ -10,23 +11,17 @@ bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 
 @dp.message_handler(commands=['start', 'help'])
-async def send_welcome(message: types.update):
+async def send_welcome(message: types.CallbackQuery):
     """
     This handler will be called when user sends `/start` or `/help` command
     """
     print(message)
     await message.answer(
-        "Добро пожаловать в MeChecker. MeChecker поможет Вам определить уровень тревоги, наличие депрессии, "
-        "а также даст несколько советов о том, как привести дела в порядок. Помните, что оффлайн врача не заменит"
-        " ни один онлайн бот, поэтому обязательно проконсультируйтесь с психологом/психотерапевтом."
-        "\nДля старта теста нажмите /starttest")
-    await message.answer_sticker("https://media0.giphy.com/media/5s4Jc4DqpjsJO/giphy.gif","")
+        "Добро пожаловать в MeChecker.\nMeChecker поможет Вам определить уровень тревоги, наличие депрессии, "
+        "а также даст несколько советов о том, как привести дела в порядок. \nПомните, что оффлайн врача не заменит"
+        " ни один онлайн бот, поэтому обязательно проконсультируйтесь с психологом или психотерапевтом.")
 
-
-
-@dp.message_handler(content_types=[types.ContentType.ANY])
-async def echo(message: types.update):
-    print("")
+    await message.answer_sticker("https://media0.giphy.com/media/5s4Jc4DqpjsJO/giphy.gif","", reply_markup=button_start)
 
 
 if __name__ == '__main__':
